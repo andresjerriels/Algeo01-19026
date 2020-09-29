af97cb9a2daceeda2013089148bdd7bbc35d94f0
gdeananthap <gdeanantha33@gmail.com>
2020-09-29 23:15:24 +0700

    update driver

--- src/MatriksInit.java
+++ src/MatriksInit.java
@@ -14,68 +14,77 @@ public class MatriksInit {
 	MatriksInit(int pilihan){
 		
 		scan = new Scanner(System.in);
-		System.out.println ("Pilih salah satu cara input di bawah ini: ");
-        System.out.println ("1. Baca dari keyboard");
-        System.out.println ("2. Baca dari file yang sudah ada");
+		System.out.println ("#=============================================================================================#");
+		System.out.println ("# Pilih salah satu cara input di bawah ini: 	                                              #");
+		System.out.println ("#=============================================================================================#");
+        System.out.println ("# 1. Baca dari keyboard                                                                       #");
+        System.out.println ("# 2. Baca dari file yang sudah ada                                                            #");
         boolean inputSuccess = false;
         
         while (!inputSuccess) {
         	int caraInput = scan.nextInt();
         	if (caraInput == 1) {
         		if (pilihan==1) {
-        			matrixInitKeyboard();
-        			System.out.println ("Masukkan elemen-elemen matriks A, dengan urutan: ");
-        			System.out.println ("a11 a12 a13 ... a1m");
-        			System.out.println ("a21 a22 a23 ... a2m");
-        			System.out.println ("...");
-        			System.out.println ("an1 an2 an3 ... anm");
+					matrixInitKeyboard();
+					System.out.println ("#=============================================================================================#");
+        			System.out.println ("# Masukkan elemen-elemen matriks A, dengan urutan:                                            #");
+        			System.out.println ("# a11 a12 a13 ... a1m                                                                         #");
+        			System.out.println ("# a21 a22 a23 ... a2m                                                                         #");
+        			System.out.println ("# ...                                                                                         #");
+        			System.out.println ("# an1 an2 an3 ... anm                                                                         #");
                 	enterMatrix(scan, this.NBrsEff, this.NKolEff-1);
-                	System.out.println ("Masukkan elemen-elemen matriks b, dengan urutan: ");
-                	System.out.println ("b1");
-                	System.out.println ("b2");
-                	System.out.println ("...");
-                	System.out.println ("bn");
+					System.out.println ("#=============================================================================================#");
+					System.out.println ("# Masukkan elemen-elemen matriks b, dengan urutan:                                            #");
+                	System.out.println ("# b1                                                                                          #");
+                	System.out.println ("# b2                                                                                          #");
+                	System.out.println ("# ...                                                                                         #");
+                	System.out.println ("# bn                                                                                          #");
                 	enterSolution(scan);
                 	inputSuccess = true;
         		}
         		else if (pilihan==2 || pilihan==3) {
-        			matrixInitKeyboard2();
-        			System.out.println ("Masukkan elemen-elemen matriks A, dengan urutan: ");
-        			System.out.println ("a11 a12 a13 ... a1n");
-        			System.out.println ("a21 a22 a23 ... a2n");
-        			System.out.println ("...");
-        			System.out.println ("an1 an2 an3 ... ann");
+					matrixInitKeyboard2();
+					System.out.println ("#=============================================================================================#");
+        			System.out.println ("# Masukkan elemen-elemen matriks A, dengan urutan:                                            #");
+        			System.out.println ("# a11 a12 a13 ... a1m                                                                         #");
+        			System.out.println ("# a21 a22 a23 ... a2m                                                                         #");
+        			System.out.println ("# ...                                                                                         #");
+        			System.out.println ("# an1 an2 an3 ... anm                                                                         #");
                 	enterMatrix(scan, this.NBrsEff, this.NKolEff);
                 	inputSuccess = true;
         		}
         		
         		else if (pilihan==4) {
-        			matrixInitKeyboard3();
-        			System.out.println ("Masukkan pasangan titik x dan y sebanyak n, dengan format: ");
-        			System.out.println ("x1 y1");
-        			System.out.println ("x2 y2");
-        			System.out.println ("...");
-        			System.out.println ("xn yn");
+					matrixInitKeyboard3();
+					System.out.println ("#=============================================================================================#");
+        			System.out.println ("# Masukkan pasangan titik x dan y sebanyak n, dengan format:                                  #");
+        			System.out.println ("# x1 y1                                                                                       #");
+        			System.out.println ("# x2 y2                                                                                       #");
+        			System.out.println ("# ...                                                                                         #");
+        			System.out.println ("# xn yn                                                                                       #");
                 	enterMatrix(scan, this.NBrsEff, this.NKolEff);
                 	inputSuccess = true;
         		}
         		
         		else if (pilihan==5) {
         			matrixInitKeyboard4();
-        			System.out.println ("Masukkan persamaan-persamaannya dalam bentuk: ");
-        			System.out.println ("y1 x11 x12 ... x1n");
-        			System.out.println ("y2 x21 x22 ... x2n");
-        			System.out.println ("...");
-        			System.out.println ("yn xn1 xn2 ... xnn");
+					System.out.println ("#=============================================================================================#");
+					System.out.println ("# Masukkan persamaan-persamaannya dalam bentuk:                                               #");
+        			System.out.println ("# y1 x11 x12 ... x1n                                                                          #");
+        			System.out.println ("# y2 x21 x22 ... x2n                                                                          #");
+        			System.out.println ("# ...                                                                                         #");
+        			System.out.println ("# yn xn1 xn2 ... xnn                                                                          #");
                 	enterMatrix(scan, this.NBrsEff, this.NKolEff);
                 	inputSuccess = true;
         		}
         		
             	
             } else if (caraInput == 2){
-            	System.out.println("Masukkan nama file: ");
-            	Scanner fileNameScanner = new Scanner(System.in);
-            	fileName = fileNameScanner.next();
+				System.out.println("#=============================================================================================#");
+				System.out.println("# Masukkan nama File berisi matriks dengan format nama_folder/nama_file.txt:                  #");
+				System.out.println("# Contoh test_case/tc1.txt                                                                    #");
+				Scanner fileNameScanner = new Scanner(System.in);
+            	fileName = "../"+fileNameScanner.next();
 //            	fileNameScanner.close();
             	openFile(fileName);
         		matrixInitFile();
@@ -83,7 +92,7 @@ public class MatriksInit {
 //        		closeFile();
         		inputSuccess = true;
             } else {
-            	System.out.println("Pilihan tidak valid, silakan coba lagi");
+            	System.out.println("# Pilihan tidak valid, silakan coba lagi                                                      #");
             }
         }
         
@@ -127,9 +136,10 @@ public class MatriksInit {
 	//Matrix initialization (entry from keyboard - versi 1) - SPL
 	public void matrixInitKeyboard() {
 		scan = new Scanner(System.in);
-		System.out.println("Masukkan jumlah baris matriks: ");
+		System.out.println("#=============================================================================================#");
+		System.out.println("# Masukkan jumlah baris matriks:                                                              #");
 	    this.NBrsEff = scan.nextInt();
-	    System.out.println("Masukkan jumlah kolom matriks: ");
+	    System.out.println("# Masukkan jumlah kolom matriks:                                                              #");
 	    this.NKolEff = scan.nextInt()+1;
 	    
 	    this.matrix = new double[NBrsEff][NKolEff];
@@ -138,7 +148,7 @@ public class MatriksInit {
 	//Matrix initialization (entry from keyboard - versi 2) - determinan, invers
 	public void matrixInitKeyboard2() {
 		scan = new Scanner(System.in);
-		System.out.println("Masukkan n: ");
+		System.out.println("# Masukkan n:                                                                                 #");
 	    this.NBrsEff = scan.nextInt();
 	    this.NKolEff = this.NBrsEff;
 	    
@@ -148,7 +158,7 @@ public class MatriksInit {
 	//Matrix initialization (entry from keyboard - versi 3) - interpolasi
 	public void matrixInitKeyboard3() {
 		scan = new Scanner(System.in);
-		System.out.println("Masukkan n: ");
+		System.out.println("# Masukkan n:                                                                                 #");
 	    this.NBrsEff = scan.nextInt();
 	    this.NKolEff = 2;
 	    
@@ -158,9 +168,9 @@ public class MatriksInit {
 	//Matrix initialization (entry from keyboard - versi 4) - regresi
 		public void matrixInitKeyboard4() {
 			scan = new Scanner(System.in);
-			System.out.println("Masukkan jumlah peubah (n): ");
+			System.out.println("# Masukkan jumlah peubah (n):                                                                 #");
 			this.NKolEff = scan.nextInt()+1;
-			System.out.println("Masukkan banyaknya data (i): ");
+			System.out.println("# Masukkan banyaknya data (i):                                                                #");
 		    this.NBrsEff = scan.nextInt();
 		    
 		    this.matrix = new double[NBrsEff][NKolEff];
